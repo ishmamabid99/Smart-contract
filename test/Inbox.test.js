d9d81655d5cc77e7050592d42fd9a10f6f69879c
ishmamabid99 <ishmam.abid5422@gmail.com>
2021-02-13 19:17:49 +0600

    function changed added

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,18 +8,29 @@ const web3= new Web3(ganache.provider());
 let accounts;
 let inbox ;
 
+
+
 beforeEach(async () =>{
   //get a list of all accounts
     accounts = await web3.eth.getAccounts();
   // using one of these accounts to deploy the Contracts
 
     inbox=  await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data : bytecode , arguments: ['Hi there!'] })
+    .deploy({data : bytecode , arguments: ['Hi There!']})
     .send({from : accounts[0], gas: '1000000'});
 });
 
 describe('Inbox',() =>{
   it('deploys a contract' , () =>{
-    console.log (inbox);
+    assert.ok(inbox.options.address);  
+  });
+  it ('it has a default message',async ()=>{
+    const message = await inbox.methods.message().call();
+    assert.equal(message , 'Hi There!');
+  });
+  it ('can change message' , async()=>{
+    await inbox.methods.setMessage('bye').send({from : accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message , 'bye');
   });
 });
